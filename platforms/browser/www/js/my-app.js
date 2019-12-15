fcfb0c2d19c10ebb78f1a42c2654b3bfcd880c24
Daniel Murphy <danielmurphy50@gmail.com>
2019-12-15 22:25:43 +0000

    first commit

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -17,28 +17,22 @@ var mainView = myApp.addView('.view-main', {
 $$(document).on('deviceready', function () {
     console.log("Device is ready!");
     navigator.geolocation.getCurrentPosition(geoCallback, onError);
-
 });
 
-/*myApp.onPageInit('index', function (page) {
-    var url_string = window.location.href;
-    var url = new URL(url_string);
-    console.log(url_string);
-    var c = url.searchParams.get("register");
-    console.log(c);
-    if(c != null){
-        document.getElementById("messageDisplay").innerHTML = "Successfully logged in";
-    }
-    
-});
-*/
+//Google map object
 var map;
+//Current user details
 var User;
+//html silder
+var distanceSilder;
+//current value of the distnace silder
+var silderValue;
+//Current location of the user
 var currentLocation = {
     "lat": 0,
     "lng": 0
 };
-
+//Format of registration request
 var RegistrationFormat = {
     "userPubInfo": {
         "username": "",
@@ -58,7 +52,7 @@ var RegistrationFormat = {
     }
 
 };
-
+//Format of login request
 var LoginFormat = {
     "email": "",
     "password": "",
@@ -68,32 +62,88 @@ var LoginFormat = {
     }
 };
 
-
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
-
-
-});
-
-myApp.onPageInit('RoommateList', function (page) {
-    FindRoommatesRequest(100, currentLocation.lat, currentLocation.lng);
-
-    map = new google.maps.Map(document.getElementById('map'),
-        {
-            zoom: 15,
-            center: currentLocation
-        }
-    );
-
-});
-
-
+//Server endpoints
 var ServerValues = {
     loginEndpoint: "http://127.0.0.1:8081/login",
     registrationEndpoint: "http://127.0.0.1:8081/registration",
     getRoommatesEndpoint: "http://127.0.0.1:8081/getRoommates"
+};
 
+//Run when the roommateList page is loaded
+myApp.onPageInit('RoommateList', function (page) {
+    ClearErrorDisplay();
+
+    //Creates a new map 
+    map = new google.maps.Map(document.getElementById('map'),
+        {
+            zoom: 13,
+            center: currentLocation
+        }
+    );
+    //Adds current location
+    addMarkerToMap(currentLocation.lat, currentLocation.lng, "red");
+
+    //set distance Silder
+    distanceSilder = document.getElementById("distanceSilder");
+    //set silder value
+    silderValue = document.getElementById("silderValue");
+    silderValue.innerHTML = distanceSilder.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    distanceSilder.oninput = function () {
+        silderValue.innerHTML = this.value;
+    };
+  
+    FindRoommates();
+});
+
+//Run when the registration page is loaded
+myApp.onPageInit('registration', function (page) {
+    ClearErrorDisplay();
+});
+
+//Run when the login page is loaded
+myApp.onPageInit('login', function (page) {
+    ClearErrorDisplay();
+    console.log("login page");
+});
+
+//Run when the index page is loaded
+myApp.onPageInit('index', function (page) {
+    ClearErrorDisplay();
+    console.log("Index page");
+});
+
+
+
+function SendPostRequest(url,content,successCallback){
+    var url = url;
+    var http = new XMLHttpRequest();
+    // Preparing the request
+    http.open("POST", url, true);
+    http.setRequestHeader('Content-Type', 'application/json');
+    // Sending the request
+    http.send(JSON.stringify(content));
+
+    // Called when we get a response
+    http.onreadystatechange = (e) => {
+        // Getting the response in a text format
+        var response = http.response;
+        // converting the response from a text format to a json format
+        console.log(response);
+
+        try {
+            var responseJSON = JSON.parse(response);
+            successCallback(responseJSON);
+        }
+        catch (err) {
+            console.log("Post Request Error " + err.message);
+        }
+
+    }
 }
+
+
 
 function Login() {
 
@@ -102,43 +152,21 @@ function Login() {
     LoginFormat.location.latitude = currentLocation.lat;
     LoginFormat.location.longitude = currentLocation.lng;
 
-    SendLoginRequest(LoginFormat, LoginCallback);
+    SendPostRequest(ServerValues.loginEndpoint,LoginFormat, LoginCallback,LoginErrorCallback);
 }
 
-function SendLoginRequest(LoginFormat, LoginCallback) {
-    var url = ServerValues.loginEndpoint;
-    var http = new XMLHttpRequest();
-    // Preparing the request
-    http.open("POST", url, true);
-    http.setRequestHeader('Content-Type', 'application/json');
-    // Sending the request
-    http.send(JSON.stringify(LoginFormat));
-
-    // Called when we get a response
-    http.onreadystatechange = (e) => {
-        // Getting the response in a text format
-        var response = http.response;
-        // converting the response from a text format to a json format
-
-        try {
-            var responseJSON = JSON.parse(response);
-            LoginCallback(responseJSON);
-        }
-        catch (err) {
-            console.log(err.message);
-        }
-
-    }
-
+function LoginErrorCallback(){
+    ShowErrorMessage("Login was unsuccessful");
 }
+
 
 var LoginCallback = function (JsonObject) {
 
     if (JsonObject.length >= 1) {
-        User = JsonObject;
+        User = JsonObject[0];
         mainView.router.loadPage({ url: 'RoommateList.html', ignoreCache: true, reload: true });
     } else {
-        document.getElementById("errorDisplay").innerHTML = "Your credentails are wrong";
+        ShowErrorMessage("Your profile could not be found. Check your email and password");
     }
 }
 
@@ -154,88 +182,59 @@ function Register() {
     RegistrationFormat.userPubInfo.currentLocation.longitude = currentLocation.lng;
     RegistrationFormat.userPriInfo.password = document.getElementById("regPassword").value;
 
-    SendRegisterRequest(RegistrationFormat, RegistrationCallback);
-
+    SendPostRequest(ServerValues.registrationEndpoint,RegistrationFormat, RegistrationCallback);
 }
-
-
-function geoCallback(position) {
-    console.log("here");
-    currentLocation = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-    }
-
-    console.log(currentLocation);
-}
-
-function onError(message) {
-    console.log("error Message:" + message);
-}
-
-
-function SendRegisterRequest(RegistrationFormat, RegistrationCallback) {
-    var url = ServerValues.registrationEndpoint;
-    var http = new XMLHttpRequest();
-    // Preparing the request
-    http.open("POST", url, true);
-    http.setRequestHeader('Content-Type', 'application/json');
-    // Sending the request
-
-    http.send(JSON.stringify(RegistrationFormat));
-
-    // Called when we get a response
-    http.onreadystatechange = (e) => {
-        // Getting the response in a text format
-        var response = http.response;
-        // converting the response from a text format to a json format
-
-        try {
-            var responseJSON = JSON.parse(response);
-
-            RegistrationCallback(responseJSON);
-        }
-        catch (err) {
-            console.log("Error" + err.message);
-        }
-
-
-
-    }
-}
-
-
-
 
 var RegistrationCallback = function (JsonObject) {
     if (JsonObject == true) {
         mainView.router.loadPage({ url: 'index.html', ignoreCache: true, reload: true });
+    } else {
+        ShowErrorMessage("Registration Failed! Check your details");
     }
-
 }
 
+function geoCallback(position) {
+    currentLocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+    }
+}
+
+function onError(message) {
+    
+    console.log("error Message:" + message);
+}
+
+function FindRoommates() {
+    map = new google.maps.Map(document.getElementById('map'),
+        {
+            zoom: 13,
+            center: currentLocation
+        }
+    );
+
+    addMarkerToMap(currentLocation.lat,currentLocation.lng,"red");
+    FindRoommatesRequest(distanceSilder.value, currentLocation.lat, currentLocation.lng);
+}
 
 
 function FindRoommatesRequest(range, currentlatitude, currentlongitude) {
     var url = ServerValues.getRoommatesEndpoint + "?range=" + range + "&userLatitude=" + currentlatitude + "&userLongitude=" + currentlongitude;
     var http = new XMLHttpRequest();
-    var response
     // Preparing the request
     http.open("GET", url, true);
     http.setRequestHeader('Content-Type', 'application/json');
 
     // Sending the request
-
     http.send();
 
 
     // Called when we get a response
     http.onreadystatechange = (e) => {
+
         // Getting the response in a text format
-        response = http.response;
-        // converting the response from a text format to a json format
-        var responseJSON = JSON.parse(response);
-        console.log(response);
+        var response = http.response;
+
         try {
             var responseJSON = JSON.parse(response);
             PopulateHtmlWithRoommates(responseJSON);
@@ -244,34 +243,53 @@ function FindRoommatesRequest(range, currentlatitude, currentlongitude) {
             console.log("Error:" + err.message)
         }
 
+
     }
 
 
 }
 
 function PopulateHtmlWithRoommates(Roommates) {
-
-    if (Roommates.length > 0) {
-
-
-        var filteredRoommates = FilterRoommates(Roommates);
+    var filteredRoommates = FilterRoommates(Roommates);
+    document.getElementById("roommates").innerHTML="";
+    if (filteredRoommates.length > 0) {
+        
         var html = "";
-        console.log("Roomates: ");
-        console.log(Roommates);
+    
         for (var i = 0; i < filteredRoommates.length; i++) {
-            updateMap(filteredRoommates[i].currentLocation.latitude, filteredRoommates[i].currentLocation.longitude);
+            addMarkerToMap(filteredRoommates[i].currentLocation.latitude, filteredRoommates[i].currentLocation.longitude, "green");
 
             html += '<div class="Roommate">'
-                + '<p class="RoommateName">' + filteredRoommates[i].username + '</p>'
-                + '<p class="RoommateAge">' + filteredRoommates[i].age + '</p>'
-                + '<p class="RoommateLatitude">' + filteredRoommates[i].currentLocation.latitude + '</p>'
-                + '<p class="RoommateLongitude">' + filteredRoommates[i].currentLocation.longitude + '</p>'
-                + '</div>';
-        }
+                + '<p>' + (i+1) + '.</p>'
+                + '<table>'
+                + '<tr>'
+                + ' <td>Name</td>'
+                + '<td>' + filteredRoommates[i].username + '</td>'
 
+                + '</tr>'
+                + '<tr>'
+                + '<td>Age</td>'
+                + '<td>' + filteredRoommates[i].age + '</td>'
+
+                + '</tr>'
+                + '<tr>'
+                + ' <td>Latitude</td>'
+                + '<td>' + filteredRoommates[i].currentLocation.latitude + '</td>'
+
+                + '</tr>'
+                + '<tr>'
+                + '<td>Longitude</td>'
+                + '<td>' + filteredRoommates[i].currentLocation.longitude + '</td>'
+
+                + '</tr>'
+                + '</table>'
+                + '</div>';
+
+        }
+        
         document.getElementById("roommates").innerHTML = html;
     } else {
-        console.log("There was no roommates in range");
+        ShowErrorMessage("There was no roommates in range");
     }
 
 }
@@ -280,13 +298,7 @@ function FilterRoommates(Roommates) {
     var filteredRoommates = [];
 
     for (var i = 0; i < Roommates.length; i++) {
-        console.log(User);
-        console.log(Roommates[i].age + " " + User[0].age + " " + User[0].smoker);
-        console.log(Roommates[i].smoker == User[0].smoker);
-        console.log((Roommates[i].age + 10) >= User[0].age);
-        console.log((Roommates[i].age - 10) <= User[0].age)
-        if ((Roommates[i].smoker == User[0].smoker) && ((Roommates[i].age + 10) >= User[0].age) && ((Roommates[i].age - 10) <= User[0].age)) {
-            console.log("Filtered:" + Roommates[i]);
+        if ((Roommates[i].username != User.username) && (Roommates[i].smoker == User.smoker) && ((Roommates[i].age + 10) >= User.age) && ((Roommates[i].age - 10) <= User.age)) {
             filteredRoommates.push(Roommates[i]);
         }
     }
@@ -295,15 +307,31 @@ function FilterRoommates(Roommates) {
 }
 
 
-function updateMap(lat, long) {
+function addMarkerToMap(lat, long, color) {
     var position = { lat: lat, lng: long };
-
-
     var marker = new google.maps.Marker({
         position: position,
-        map: map
+        map: map,
+        icon: 'http://maps.google.com/mapfiles/ms/icons/' + color + '-dot.png'
     });
 }
+
+function ShowErrorMessage(message) {
+    document.getElementById("ErrorPopUp").style.visibility = "visible";
+    document.getElementById("ErrorPopUpMessage").innerHTML = message;
+}
+
+function ClearErrorDisplay() {
+    document.getElementById("ErrorPopUp").style.visibility = "hidden";
+    document.getElementById("ErrorPopUpMessage").innerHTML = "";
+}
+
+function BackToIndex() {
+    mainView.router.loadPage({ url: 'index.html', ignoreCache: true, reload: true });
+}
+
+
+
 
 
 
